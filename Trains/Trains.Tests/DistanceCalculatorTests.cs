1a106c783082c420b0f4934aed88a9f680e032c6
daniel watts <danielw@.7digital.local>
2014-03-01 16:35:39 +0000

    move tests out to their own project and start working on finding the number of routes

--- Trains/Trains.Tests/DistanceCalculatorTests.cs
+++ Trains/Trains.Tests/DistanceCalculatorTests.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
-namespace Trains
+namespace Trains.Tests
 {
 	[TestFixture]
 	public class DistanceCalculatorTests
---
+++ Trains/Trains.Tests/NumberOfRoutesTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace Trains.Tests
+{
+	[TestFixture]
+	public class NumberOfRoutesTests
+	{
+		 
+	}
+}
