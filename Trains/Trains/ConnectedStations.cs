b0bf397c38f9db4674db704382b2a1c23692466a
daniel watts <danielw@.7digital.local>
2014-03-01 20:45:09 +0000

    move connectedstations class into seperate file

---
+++ Trains/Trains/ConnectedStations.cs
@@ -0,0 +1,16 @@
+﻿namespace Trains
+{
+	public class ConnectedStations
+	{
+		public ConnectedStations(string start, string end, int distance)
+		{
+			StartStation = start;
+			EndStation = end;
+			Distance = distance;
+		}
+
+		public string StartStation { get; set; }
+		public string EndStation { get; set; }
+		public int Distance { get; set; }
+	}
+}
--- Trains/Trains/Route.cs
+++ Trains/Trains/Route.cs
@@ -11,18 +11,4 @@ namespace Trains
 			return route;
 		}
 	}
-
-	public class ConnectedStations
-	{
-		public ConnectedStations(string start, string end, int distance)
-		{
-			StartStation = start;
-			EndStation = end;
-			Distance = distance;
-		}
-
-		public string StartStation { get; set; }
-		public string EndStation { get; set; }
-		public int Distance { get; set; }
-	}
 }
